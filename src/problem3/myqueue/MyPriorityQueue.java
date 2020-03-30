71198564f0928ab66f8c46611883319834fc3a49
himanshusingh11 <himanshu.singh_iot18@gla.ac.in>
2020-03-30 22:36:36 +0530

    my priority queue completed

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -6,5 +6,58 @@
  */
 package problem3.myqueue;
 
+import problem3.node.Node;
+import problem5.student.Student;
+
 public class MyPriorityQueue {
+    Node head;
+
+    public void enqueue(Student student) {
+        Node node = new Node();
+        node.setStudent(student);
+        //node.setNext(null);
+        node.setPriority(student.rollNo);
+        if (head == null) {
+            head = node;
+        } else {
+            Node iterator = head;
+            if (head.getPriority() > node.getPriority()) {
+                Node temp = head;
+                head = node;
+                head.setNext(temp);
+            } else {
+                while (iterator.getPriority() < node.getPriority()) {
+                    if (iterator.getNext() == null) {
+                        iterator.setNext(node);
+                    } else if (node.getPriority() < iterator.getNext().getPriority()) {
+                        node.setNext(iterator.getNext());
+                        iterator.setNext(node);
+                    } else {
+                        iterator = iterator.getNext();
+                    }
+                }
+            }
+        }
+    }
+
+    public Node dequeue() {
+        Node temp = head;
+        head = temp.getNext();
+        return temp;
+    }
+
+    public Node peek() {
+        return head;
+    }
+
+    public void displayAll() {
+        Node temp = head;
+        while (temp.getNext() != null) {
+            Student s = temp.getStudent();
+            System.out.println(s.name + "  " + s.rollNo);
+            temp = temp.getNext();
+        }
+    }
+
+
 }
