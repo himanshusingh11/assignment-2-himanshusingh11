bc048d7e3a6a41c981310641c153130bfafb6483
himanshusingh11 <himanshu.singh_iot18@gla.ac.in>
2020-03-30 22:47:42 +0530

    node class of problem 5

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -1,10 +1,35 @@
-/*
+package problem5.node;/*
  *  Created by IntelliJ IDEA.
  *  User: Vaibhav
  *  Date: 23-Mar-20
  *  Time: 7:06 PM
  */
-package problem5.node;
+
+import problem5.student.Student;
+
 // to define node properties
 public class Node {
+    private Student data;
+    private Node next;
+
+    public Node(Student data) {
+        this.data = data;
+        next = null;
+    }
+
+    public Student getData() {
+        return data;
+    }
+
+    public void setData(Student data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
