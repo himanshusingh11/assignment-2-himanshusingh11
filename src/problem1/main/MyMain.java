652918b5c192846bb8757028940b52d5123a81c2
himanshusingh11 <himanshu.singh_iot18@gla.ac.in>
2020-03-30 22:20:32 +0530

    created main class with all methods

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -6,6 +6,23 @@
  */
 package problem1.main;
 // executable class
+import problem1.mybst.MyBinarySearchTree;
 
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree();
+        int[] insertData = {13, 3, 4, 12, 14, 10, 5, 1, 8, 2, 7, 9, 11, 6, 18, 15};
+        for (int a :
+                insertData) {
+            myBinarySearchTree.insert(a);
+            System.out.print("Inserted : " + a + "  ");
+        }
+        System.out.println("\nTree Traversal : ");
+        myBinarySearchTree.traverse(myBinarySearchTree.root);
+        System.out.println("\nTraversal of all Left Children : ");
+        myBinarySearchTree.traverseLeftChildren(myBinarySearchTree.root);
+        System.out.print("\nNumber of  nodes without left child : ");
+        myBinarySearchTree.noOfNodesWithoutLeftChild(myBinarySearchTree.root);
+
+    }
 }
