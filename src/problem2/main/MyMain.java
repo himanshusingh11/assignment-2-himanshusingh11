4855dc87d226ced9f16c81037683ee2594442726
himanshusingh11 <himanshu.singh_iot18@gla.ac.in>
2020-03-30 22:28:09 +0530

    problem 2 solved

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -7,5 +7,21 @@
 package problem2.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
+
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree();
+        int[] insertData = {13, 3, 4, 12, 14, 10, 5, 1, 8, 2, 7, 9, 11, 6, 18};
+        for (int a :
+                insertData) {
+            myBinarySearchTree.insert(a);
+        }
+        System.out.println("Preorder Traversal :");
+        myBinarySearchTree.preOrder(myBinarySearchTree.root);
+        System.out.println("\nPostorder Traversal :");
+        myBinarySearchTree.postOrder(myBinarySearchTree.root);
+
+    }
 }
