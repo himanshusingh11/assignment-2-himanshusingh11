6d0512734fd24cfa4711ae66717065e0eff07b10
himanshusingh11 <himanshu.singh_iot18@gla.ac.in>
2020-03-30 22:49:09 +0530

    enqueue method

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -5,6 +5,32 @@
  *  Time: 7:06 PM
  */
 package problem5.circularqueue;
+
+import problem5.node.Node;
+import problem5.student.Student;
+
 //to implement circular queue
 public class MyCircularQueue {
+    private Node rear;
+    private int size;
+
+    public MyCircularQueue() {
+        rear = null;
+        size = 0;
+    }
+
+    public void enqueue(Student data) {
+        Node node = new Node(data);
+        if (isEmpty()) {
+            rear = node;
+            size++;
+            node.setNext(node);
+        } else {
+            node.setNext(rear.getNext());
+            rear.setNext(node);
+            rear = node;
+            size++;
+        }
+    }
+
 }
