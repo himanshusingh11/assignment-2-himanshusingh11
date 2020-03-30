1ff0d982d085c55c4f6e15468fe2d5fed16fa07f
himanshusingh11 <himanshu.singh_iot18@gla.ac.in>
2020-03-30 22:44:19 +0530

    created method enQueue

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -8,5 +8,20 @@ package problem4.myqueue;
 // to create queue to store pre - order successor
 
 public class MyQueue {
+    QueueNode root;
 
+    public void enQueue(int data) {
+        QueueNode newInput = new QueueNode();
+        newInput.setData(data);
+        newInput.setNext(null);
+        if (root == null) {
+            root = newInput;
+        } else {
+            QueueNode temp = root;
+            while (temp.getNext() != null) {
+                temp = temp.getNext();
+            }
+            temp.setNext(newInput);
+        }
+    }
 }
