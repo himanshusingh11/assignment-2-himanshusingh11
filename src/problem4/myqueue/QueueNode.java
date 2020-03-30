d7493e00fca62324ae4fbd8ca4d0402817a0d8f1
himanshusingh11 <himanshu.singh_iot18@gla.ac.in>
2020-03-30 22:42:43 +0530

    added class variables with getter and setter

--- src/problem4/myqueue/QueueNode.java
+++ src/problem4/myqueue/QueueNode.java
@@ -1,4 +1,22 @@
 package problem4.myqueue;
 
 public class QueueNode {
+    private int data;
+    private QueueNode next;
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public QueueNode getNext() {
+        return next;
+    }
+
+    public void setNext(QueueNode next) {
+        this.next = next;
+    }
 }
