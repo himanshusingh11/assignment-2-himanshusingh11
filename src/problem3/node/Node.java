8bb458bcd42020fbcc615cc68af24fab597c2340
himanshusingh11 <himanshu.singh_iot18@gla.ac.in>
2020-03-30 22:32:44 +0530

    created node class for problem 3

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -6,5 +6,35 @@
  */
 package problem3.node;
 
+import problem5.student.Student;
+
 public class Node {
+    private Node next;
+    private Student student;
+    private int priority;
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
 }
