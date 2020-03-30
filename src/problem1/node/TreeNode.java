133bae9ba7a2565fe9b3c8a667bf87caa5f7bd9d
himanshusingh11 <himanshu.singh_iot18@gla.ac.in>
2020-03-30 22:08:04 +0530

    added tree node class

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -7,4 +7,35 @@
 package problem1.node;
 
 public class TreeNode {
+    int data;
+    private TreeNode leftChild;
+    private TreeNode rightChild;
+
+    public TreeNode(int data) {
+        this.data = data;
+    }
+
+    public TreeNode getLeftChild() {
+        return leftChild;
+    }
+
+    public void setLeftChild(TreeNode leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public TreeNode getRightChild() {
+        return rightChild;
+    }
+
+    public void setRightChild(TreeNode rightChild) {
+        this.rightChild = rightChild;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
 }
