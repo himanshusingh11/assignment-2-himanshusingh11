c745f2887bc9b3c7fa83a6cc8461eb1f9d2d4ac2
himanshusingh11 <himanshu.singh_iot18@gla.ac.in>
2020-03-30 22:22:38 +0530

    student class created

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -5,6 +5,19 @@
  *  Time: 7:06 PM
  */
 package problem5.student;
+
 // to store student information and properties
 public class Student {
+
+    public String name;
+    public String course;
+    public int rollNo;
+    public int backlogs;
+
+    public Student(String name, String course, int rollNo) {
+        this.name = name;
+        this.course = course;
+        this.rollNo = rollNo;
+    }
 }
+
